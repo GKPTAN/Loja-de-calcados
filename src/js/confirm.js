7753afa4f1c65f0137ca3c6baf98e83f6aef8161
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-06-14 17:20:50 -0300

    efeito do botão
    - efeito do botão enviar e-mail finalizado

---
+++ src/js/confirm.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const emailInput = document.getElementById('email-usuario');
+    const botaoEnviar = document.getElementById('button');
+
+    //console.log("código carregado");
+
+    emailInput.addEventListener('input', function () {
+        const emailValue = emailInput.value;
+
+        //console.log("mudança no input email");
+
+        if(validarEmail(emailValue)) {
+            //console.log("e-mail é válido");
+            botaoEnviar.classList.remove('desativado');
+        } else {
+            //console.log("e-mail inválido");
+            botaoEnviar.classList.add('desativado');
+        };
+    });
+
+    botaoEnviar.addEventListener('click', function (event) {
+        //console.log("botão clicado")
+        emailInput.value = '';
+        if(botaoEnviar.classList.contains('desativado')) {
+            //console.log("botão está desabilitado")
+            event.preventDefault();
+        } else {
+            //console.log("botão está ativado")
+        }
+    });
+
+    function validarEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    };
+});
--- src/js/submit.js
+++ src/js/submit.js
@@ -5,13 +5,16 @@ $(function () {
 
   function validate() {
     setTimeout(function () {
-      $("#button").removeClass("onclic");
+      $("#button").removeClass("onclic material-symbols-outlined");
+      $("#button").text('');
       $("#button").addClass("validate", 450, callback);
     }, 2250);
   }
   function callback() {
     setTimeout(function () {
       $("#button").removeClass("validate");
+      $("#button").addClass("material-symbols-outlined desativado");
+      $("#button").text('arrow_forward');
     }, 1250);
   }
 });
