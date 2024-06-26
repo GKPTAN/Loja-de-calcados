71c2f5e8c6916f4dabfe53c5d32ec635a88069b9
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-06-25 22:04:53 -0300

    JavaScript parte 1
    - retirei a função caixa de zoom do código pois leitores de tela podem confundir como texto duplicado
    
    - adicionei um novo botão para aumentar a fonte das frases onde estão os preços em letras pequenas, coloquei um limite de 24px para que não ultrapassassem o conteúdo.

--- src/js/index.js
+++ src/js/index.js
@@ -1,69 +1,29 @@
-//tentei fazer uma lupa mas não consegui fazer do jeito que eu queria, vou tentar descobrir mais sobre isso
-const caixaDeZoom = document.getElementById('caixa-de-zoom');
-
-function mostrarCaixaDeZoom(event) {
-    const textoDoElemento = event.target;
-    const selecao = window.getSelection()
-
-    //const texto = textoDoElemento.textContent;
-    //const mouseX = event.clientX;
-    //const mouseY = event.clientY;
-
-    const range = document.createRange();
-
-    range.selectNodeContents(textoDoElemento);
-    selecao.removeAllRanges();
-    selecao.addRange(range)
-
-    //const textNode = textoDoElemento.childNodes[0];
-    //range.setStart(textNode, 0);
-    //range.setEnd(textNode, texto.length);
-
-    const rects = range.getClientRects();
-    let closestRect;
-    let closestDistance = Infinity;
-
-    for (const rect of rects) {
-        const distance = Math.abs(event.clientX - rect.left) +  Math.abs(event.clientY - rect.top);
-        if (distance < closestDistance) {
-            closestDistance = distance
-            closestRect = rect;
-        };
-    };
-
-    selecao.removeAllRanges();
-
-    if (closestRect) {
-        caixaDeZoom.textContent = textoDoElemento.textContent;
-        caixaDeZoom.style.display = 'block';
-        caixaDeZoom.style.left = `${event.pageX}px`;
-        caixaDeZoom.style.top = `${event.pageY}px`;
-    } else {
-        caixaDeZoom.style.display = 'none'
-    }
-
-    //const x = mouseX - rect.left;
-    //const y = mouseY - rect.top;
-
-    //const charIndex = Math.floor((x / rect.width) * texto.length);
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoZoom = document.getElementById('zoom');
+    const fonteTextos = document.querySelectorAll('.upFonte');
+    const maxFontSize = Number(24);
+    let limite = true;
     
-    //const palavra = getWordAt(texto, charIndex);
+    botaoZoom.addEventListener("click", () => {
+        fonteTextos.forEach(function(upFonte) {
+            let tamanhoFonteAtual = window.getComputedStyle(upFonte).fontSize;
+            let novoTamanho = parseFloat(tamanhoFonteAtual) + 2;
 
-    //if (palavra) {
-        //caixaDeZoom.textContent = palavra;
-    //} else {
-        //caixaDeZoom.style.display = 'none'
-    //}
-};
+            if (novoTamanho > maxFontSize) {
+                limite = false;
+            };
+        });
 
-function esconderCaixaDeZoom() {
-    caixaDeZoom.style.display = 'none';
-};
+        if (limite) {
+            fonteTextos.forEach(function(upFonte) {
+                let tamanhoFonteAtual = window.getComputedStyle(upFonte).fontSize;
+                let novoTamanho = parseFloat(tamanhoFonteAtual) + 2;
+                upFonte.style.fontSize = novoTamanho + "px";
+            });
 
-function getWordAt(str, pos) {
-    const left = str.slice(0, pos).search(/\S+$/);
-    const right = str.slice(pos).search(/\s/);
-    if (right < 0) return
-    str.slice(left);
-    return str.slice(left, right + pos);
-}
+            let currentPosition = parseFloat(window.getComputedStyle(botaoZoom).left);
+            let newPosition = currentPosition + (window.innerWidth * 0.022);
+            botaoZoom.style.left = newPosition + 'px';
+        };
+    });
+});
